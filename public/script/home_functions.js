414bd87d97dd8a3ffd06f25fc9682551771a4ef5
jriggs32 <79825506+jriggs32@users.noreply.github.com>
2023-03-09 13:18:49 -0500

    Updated Newsletter page and links
    03/9/2023 1:18pm newsletter page update + newsletter links update

--- public/script/home_functions.js
+++ public/script/home_functions.js
@@ -7,33 +7,6 @@ var viewCartButton = document.querySelector('.cart-button');//.style.cursor = "p
 
   document.getElementById("year").innerHTML = new Date().getFullYear();
 
- // Get the modal
-var modal = document.getElementById("modal-overlay");
-
-// Get the link that opens the modal
-var btn = document.getElementById("myLink");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("modal-close")[0];
-
-// When the user clicks the button, open the modal 
-if (btn != null) {
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
 
 $( ".change" ).on("click", function() {
   if( $( "body" ).hasClass( "dark" )) {
