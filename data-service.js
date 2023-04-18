d8bb58b720f2ebcad8d9ce43f146f795e43b3eb1
CoreyOaf <coreyoaf@hotmail.com>
2023-04-18 15:29:57 -0400

    Updated data-service a bit more

--- data-service.js
+++ data-service.js
@@ -1,7 +1,7 @@
 var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 //connect to Shawns MongoDb Atlas Database
-//mongoose.connect();
+mongoose.connect("mongodb+srv://oafc:EweXWr2PQE14m3uK@databasepractice.e3uyu0n.mongodb.net/test?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true });
 
 //Define Employee Schema
 var employeeSchema = new Schema({
@@ -20,11 +20,23 @@ var employeeSchema = new Schema({
 
 //Define Comic Book Schema
 var comicBookSchema = new Schema({
-    imageFilePath: String,
+    image: String,
     title: String,
     universe: String,
-    year: String,
+    year: Number,
     description: String,
     price: Number,
     quantity: Number,
+});
+
+var ComicBook = mongoose.model('comicBooks', comicBookSchema);
+//create a new Comic Book
+var AmazingSpiderman = new ComicBook({
+    image: "./images/amazingspiderman.jpeg",
+    title: "Amazing Fantasy #15",
+    universe: "Marvel",
+    year: 1962,
+    description: "The First Appearance of the Amazing Spider-Man! When young Peter Parker gains remarkable abilities from a radioactive spider, he must step up and try to become a hero — while also dealing with the fantastic pressures of an everyday teenager! For with great power, there must also come great responsibility!",
+    price: 4.95,
+    quantity: 3,
 });
