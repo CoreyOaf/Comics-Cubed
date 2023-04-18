2feffa03fefe8ea753a40606e0fa8515f8de9976
jriggs32 <79825506+jriggs32@users.noreply.github.com>
2023-04-18 12:37:55 -0400

    Updated and Created Employee and Comic Book Schema

--- data-service.js
+++ data-service.js
@@ -3,5 +3,28 @@ var Schema = mongoose.Schema;
 //connect to Shawns MongoDb Atlas Database
 //mongoose.connect();
 
-// Test comment for update 2
-//222222222222222
+//Define Employee Schema
+var employeeSchema = new Schema({
+    employeeNum: Number,
+    firstName: String,
+    lastName: String,
+    email: String,
+    addressStreet: String,
+    addressCity: String,
+    addressState: String,
+    addressPostal: String,
+    isManager: Boolean,
+    employeeManagerNum: Number,
+    hireDate: String,
+});
+
+//Define Comic Book Schema
+var comicBookSchema = new Schema({
+    imageFilePath: String,
+    title: String,
+    universe: String,
+    year: String,
+    description: String,
+    price: Number,
+    quantity: Number,
+});
